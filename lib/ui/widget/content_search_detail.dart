fa006f464fba2f30ab3ef18086d33e64257024e3
Thong <minhthong095@gmail.com>
2020-02-04 15:27:06 +0700

    update search detail with content

--- lib/ui/widget/content_search_detail.dart
+++ lib/ui/widget/content_search_detail.dart
@@ -1,21 +1,56 @@
+import 'package:Anifrag/config/app_color.dart';
+import 'package:Anifrag/ui/widget/casts_row.dart';
 import 'package:Anifrag/ui/widget/skuru_panel.dart';
+import 'package:Anifrag/ui/widget/text_more.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class ContentSearchDetail extends StatelessWidget {
-  static const _paddingContent = EdgeInsets.all(30);
+  static const _paddingContent = EdgeInsets.all(23);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         SkuruPanel(
-          height: 50,
+          backgroundColor: AppColor.backgroundColor,
+          titleColor: Colors.white,
+          height: 80,
           paddingLeftTitle: _paddingContent.left,
           title: 'Breaking Bad',
         ),
         Container(
-          color: Colors.red,
-          height: 100,
+          color: AppColor.backgroundColor,
+          padding: EdgeInsets.only(
+              left: _paddingContent.left,
+              right: _paddingContent.right,
+              top: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text(
+                  'Synopsis',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ),
+              ),
+              TextMoreOneWay(
+                tapReadMore: () {},
+                readMoreText: '   Read more',
+                text:
+                    '''Walter H. White is a chemistry genius, but works as a chemistry teacher in an Albequerque, New Mexico high school. w Mexico high school. Walter H. White is a chemistry genius, but works as a chemistry teacher in an Albequerque, New Mexico high school. w Mexico high school. Walter H. White is a chemistry genius, but works as a chemistry teacher in an Albequerque, New Mexico high school. w Mexico high school.''',
+              ),
+              CastsRow(),
+              SizedBox(
+                height: 100,
+                width: double.infinity,
+              )
+            ],
+          ),
         )
       ],
     );
--- lib/ui/widget/serach_view.dart
+++ lib/ui/widget/serach_view.dart
@@ -61,134 +61,136 @@ class _SearchViewState extends State<_SearchView> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-          left: 15, right: 15, top: Platform.isAndroid ? 15 : 0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(bottom: 10),
-            child: Container(
-              constraints: BoxConstraints.expand(height: 50),
-              decoration: BoxDecoration(
-                  color: AppColor.search,
-                  borderRadius: BorderRadius.all(Radius.circular(10))),
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 12),
-                    child: SvgPicture.asset(
-                      PathSvg.find,
-                      color: Colors.grey,
-                      height: 20,
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.only(
+            left: 15, right: 15, top: Platform.isAndroid ? 15 : 0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(bottom: 10),
+              child: Container(
+                constraints: BoxConstraints.expand(height: 50),
+                decoration: BoxDecoration(
+                    color: AppColor.search,
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 12),
+                      child: SvgPicture.asset(
+                        PathSvg.find,
+                        color: Colors.grey,
+                        height: 20,
+                      ),
                     ),
-                  ),
-                  Flexible(
-                    child: TextFormField(
-                      enableInteractiveSelection: false,
-                      onChanged: (text) {
-                        _blocSearchView.searchMovies(text);
-                      },
-                      cursorColor: Colors.grey,
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Search...',
-                          hintStyle: TextStyle(color: Colors.grey)),
-                      style: TextStyle(color: Colors.grey[50], fontSize: 20),
+                    Flexible(
+                      child: TextFormField(
+                        enableInteractiveSelection: false,
+                        onChanged: (text) {
+                          _blocSearchView.searchMovies(text);
+                        },
+                        cursorColor: Colors.grey,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Search...',
+                            hintStyle: TextStyle(color: Colors.grey)),
+                        style: TextStyle(color: Colors.grey[50], fontSize: 20),
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(right: 10),
-                    child: ValueListenableBuilder<bool>(
-                      valueListenable: _blocSearchView.valueNotifyIsLoading,
-                      builder: (_, isLoading, ___) {
-                        if (isLoading) {
-                          return Platform.isAndroid
-                              ? SizedBox(
-                                  height: 20,
-                                  width: 20,
-                                  child: Indicator(
-                                    strokeAndroid: 3,
-                                  ),
-                                )
-                              : Indicator();
-                        }
-                        return SizedBox.shrink();
-                      },
-                    ),
-                  )
-                ],
+                    Padding(
+                      padding: EdgeInsets.only(right: 10),
+                      child: ValueListenableBuilder<bool>(
+                        valueListenable: _blocSearchView.valueNotifyIsLoading,
+                        builder: (_, isLoading, ___) {
+                          if (isLoading) {
+                            return Platform.isAndroid
+                                ? SizedBox(
+                                    height: 20,
+                                    width: 20,
+                                    child: Indicator(
+                                      strokeAndroid: 3,
+                                    ),
+                                  )
+                                : Indicator();
+                          }
+                          return SizedBox.shrink();
+                        },
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          Flexible(
-              fit: FlexFit.loose,
-              child: StreamBuilder<
-                  dartz.Tuple2<SearchState, List<ResponseSearch>>>(
-                initialData: dartz.Tuple2(SearchState.kickoff, []),
-                stream: _blocSearchView.subjectSearchState,
-                builder: (_, snapshot) {
-                  switch (snapshot.data.value1) {
-                    case SearchState.kickoff:
-                      return GestureDetector(
-                        onTap: () {
-                          widget.onItemClick(423204);
-                        },
-                        child: MessageSearchView(
-                          message: 'Find what to watch next.',
-                        ),
-                      );
-                    case SearchState.fulfill:
-                      return NotificationListener<ScrollNotification>(
-                        onNotification: (notification) {
-                          if (notification is ScrollStartNotification) {
-                            FocusScope.of(context).unfocus();
-                          }
-                          return false;
-                        },
-                        child: ListView.separated(
-                          separatorBuilder: (_, index) {
-                            return SizedBox(
-                              height: 15,
-                              width: 1,
-                            );
+            Flexible(
+                fit: FlexFit.loose,
+                child: StreamBuilder<
+                    dartz.Tuple2<SearchState, List<ResponseSearch>>>(
+                  initialData: dartz.Tuple2(SearchState.kickoff, []),
+                  stream: _blocSearchView.subjectSearchState,
+                  builder: (_, snapshot) {
+                    switch (snapshot.data.value1) {
+                      case SearchState.kickoff:
+                        return GestureDetector(
+                          onTap: () {
+                            widget.onItemClick(423204);
                           },
-                          padding: EdgeInsets.only(bottom: 30),
-                          itemCount: snapshot.data.value2.length,
-                          itemBuilder: (_, index) {
-                            return GestureDetector(
-                              onTap: () {
-                                FocusScope.of(context).unfocus();
-                                widget.onItemClick(
-                                    snapshot.data.value2[index].id);
-                              },
-                              child: SearchItem(
-                                heightImg: _heightImgSearchItem,
-                                widthImg: _widthImgSearchItem,
-                                posterPath: _blocSearchView.baseUrlImage +
-                                    snapshot.data.value2[index].posterPath,
-                                popularity: snapshot
-                                    .data.value2[index].popularity
-                                    .toString(),
-                                runtime: snapshot.data.value2[index].runtime,
-                                title:
-                                    snapshot.data.value2[index].originalTitle,
-                                yearRelease:
-                                    snapshot.data.value2[index].releaseDate,
-                              ),
-                            );
+                          child: MessageSearchView(
+                            message: 'Find what to watch next.',
+                          ),
+                        );
+                      case SearchState.fulfill:
+                        return NotificationListener<ScrollNotification>(
+                          onNotification: (notification) {
+                            if (notification is ScrollStartNotification) {
+                              FocusScope.of(context).unfocus();
+                            }
+                            return false;
                           },
-                        ),
-                      );
-                    default:
-                      return MessageSearchView(
-                        message: 'Oh damm. We dont have that.',
-                      );
-                  }
-                },
-              ))
-        ],
+                          child: ListView.separated(
+                            separatorBuilder: (_, index) {
+                              return SizedBox(
+                                height: 15,
+                                width: 1,
+                              );
+                            },
+                            padding: EdgeInsets.only(bottom: 30),
+                            itemCount: snapshot.data.value2.length,
+                            itemBuilder: (_, index) {
+                              return GestureDetector(
+                                onTap: () {
+                                  FocusScope.of(context).unfocus();
+                                  widget.onItemClick(
+                                      snapshot.data.value2[index].id);
+                                },
+                                child: SearchItem(
+                                  heightImg: _heightImgSearchItem,
+                                  widthImg: _widthImgSearchItem,
+                                  posterPath: _blocSearchView.baseUrlImage +
+                                      snapshot.data.value2[index].posterPath,
+                                  popularity: snapshot
+                                      .data.value2[index].popularity
+                                      .toString(),
+                                  runtime: snapshot.data.value2[index].runtime,
+                                  title:
+                                      snapshot.data.value2[index].originalTitle,
+                                  yearRelease:
+                                      snapshot.data.value2[index].releaseDate,
+                                ),
+                              );
+                            },
+                          ),
+                        );
+                      default:
+                        return MessageSearchView(
+                          message: 'Oh damm. We dont have that.',
+                        );
+                    }
+                  },
+                ))
+          ],
+        ),
       ),
     );
   }
