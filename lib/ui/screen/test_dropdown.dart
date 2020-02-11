fcb4daf6ea89c3af0c3c27ec3b3c10276b702a9e
Thong <minhthong095@gmail.com>
2020-02-11 15:50:27 +0700

    update scroll controller for dropdown

--- lib/ui/screen/test_dropdown.dart
+++ lib/ui/screen/test_dropdown.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/physics.dart';
+import 'dart:math' as math;
+
+import 'package:flutter/rendering.dart';
 
 class TestDropDown extends StatelessWidget {
   final data = <int>[
@@ -120,6 +124,8 @@ class RouteVirgilAaronDropDown extends PopupRoute {
   final int seasonCout;
   final bool isBottomNotch;
   final bool isTopNotch;
+  final ScrollController _scrollController = ScrollController();
+  bool _isCompleteAutoFix = true;
 
   RouteVirgilAaronDropDown(
       {@required this.coordinateRect,
@@ -141,10 +147,40 @@ class RouteVirgilAaronDropDown extends PopupRoute {
     for (int c = 0; c < seasonCout; c++) yield _item(width, height, "1");
   }
 
+  double _goTo(double pixels, double baseHeight) {
+    final result = ((((pixels + 1) / baseHeight).round()) * baseHeight) - 1;
+    return result < 0 ? 0 : result;
+  }
+
+  bool _scrollNotification(UserScrollNotification notification) {
+    final position = _scrollController.position;
+
+    if (notification.direction == ScrollDirection.idle &&
+        _isCompleteAutoFix &&
+        position.pixels > 0 &&
+        position.pixels <= position.maxScrollExtent) {
+      final goTo = _goTo(position.pixels, coordinateRect.height);
+      _isCompleteAutoFix = false;
+      _scrollController
+          .animateTo(goTo,
+              duration: Duration(milliseconds: 100), curve: Curves.decelerate)
+          .whenComplete(() {
+        _isCompleteAutoFix = true;
+      });
+    }
+
+    return false;
+  }
+
+  @override
+  bool didPop(result) {
+    _scrollController.dispose();
+    return super.didPop(result);
+  }
+
   @override
   Widget buildPage(BuildContext context, Animation<double> animation,
       Animation<double> secondaryAnimation) {
-    final c = '';
     return GestureDetector(
       onTap: () {
         Navigator.of(context).pop();
@@ -159,41 +195,41 @@ class RouteVirgilAaronDropDown extends PopupRoute {
                 height: double.infinity,
                 color: Colors.green.withOpacity(0.5),
                 margin: EdgeInsets.only(left: coordinateRect.left),
-                child: SingleChildScrollView(
-                  physics: _DroPDownPhysics(),
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        color: Colors.yellow.withOpacity(0.5),
-                        constraints: BoxConstraints.expand(
-                            height: (coordinateRect.top - coordinateRect.height)
-                                    .abs() +
-                                (isTopNotch
-                                    ? 0
-                                    : MediaQuery.of(context).viewPadding.top)),
-                      ),
-                      ..._generateItem(seasonCout, coordinateRect.width,
-                          coordinateRect.height),
-                      Container(
-                        color: Colors.yellow.withOpacity(0.5),
-                        constraints: BoxConstraints.expand(
-                            height: MediaQuery.of(context).size.height -
-                                (coordinateRect.top +
-                                        coordinateRect.height +
-                                        (isBottomNotch
-                                            ? MediaQuery.of(context)
-                                                .viewPadding
-                                                .bottom
-                                            : 0))
-                                    .abs()),
-                      )
-                      // Container(
-                      //   color: Colors.yellow.withOpacity(0.5),
-                      //   constraints: BoxConstraints.expand(
-                      //       height: MediaQuery.of(context).size.height -
-                      //           coordinateRect.top),
-                      // )
-                    ],
+                child: NotificationListener<UserScrollNotification>(
+                  onNotification: _scrollNotification,
+                  child: SingleChildScrollView(
+                    controller: _scrollController,
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          color: Colors.yellow.withOpacity(0.5),
+                          constraints: BoxConstraints.expand(
+                              height:
+                                  (coordinateRect.top - coordinateRect.height)
+                                          .abs() +
+                                      (isTopNotch
+                                          ? 0
+                                          : MediaQuery.of(context)
+                                              .viewPadding
+                                              .top)),
+                        ),
+                        ..._generateItem(seasonCout, coordinateRect.width,
+                            coordinateRect.height),
+                        Container(
+                          color: Colors.yellow.withOpacity(0.5),
+                          constraints: BoxConstraints.expand(
+                              height: MediaQuery.of(context).size.height -
+                                  (coordinateRect.top +
+                                          coordinateRect.height +
+                                          (isBottomNotch
+                                              ? MediaQuery.of(context)
+                                                  .viewPadding
+                                                  .bottom
+                                              : 0))
+                                      .abs()),
+                        )
+                      ],
+                    ),
                   ),
                 ))),
       ),
@@ -202,68 +238,4 @@ class RouteVirgilAaronDropDown extends PopupRoute {
 
   @override
   Duration get transitionDuration => Duration(milliseconds: 600);
-}
-
-class _DroPDownPhysics extends ScrollPhysics {
-  const _DroPDownPhysics({ScrollPhysics parent}) : super(parent: parent);
-
-  @override
-  _DroPDownPhysics applyTo(ScrollPhysics ancestor) {
-    return _DroPDownPhysics(parent: buildParent(ancestor));
-  }
-
-  @override
-  double applyBoundaryConditions(ScrollMetrics position, double value) {
-    // print("applyBoundaryConditions $value");
-    // if (value < position.pixels &&
-    //     position.pixels <= position.minScrollExtent) // underscroll
-    //   return value - position.pixels;
-    // if (position.maxScrollExtent <= position.pixels &&
-    //     position.pixels < value) // overscroll
-    //   return value - position.pixels;
-    // if (value < position.minScrollExtent &&
-    //     position.minScrollExtent < position.pixels) // hit top edge
-    //   return value - position.minScrollExtent;
-    // if (position.pixels < position.maxScrollExtent &&
-    //     position.maxScrollExtent < value) // hit bottom edge
-    //   return value - position.maxScrollExtent;
-    // return 0.0;
-    // print("");
-    // print(
-    //     'applyBoundaryConditions \npixels ${position.pixels} \nminScrollExtend ${position.minScrollExtent} \nmaxScrollExtend ${position.maxScrollExtent} \nvieweportDimension ${position.viewportDimension} \nextendBefore ${position.extentBefore} \nextendsAfter ${position.extentAfter} \nextendsInside ${position.extentInside} \noutOfRange ${position.outOfRange}');
-    // print("");
-    final a = FixedScrollMetrics(
-        maxScrollExtent: position.maxScrollExtent,
-        viewportDimension: 10000,
-        pixels: position.pixels,
-        minScrollExtent: -1000,
-        axisDirection: position.axisDirection);
-    return super.applyBoundaryConditions(position, value);
-  }
-
-  @override
-  double applyPhysicsToUserOffset(ScrollMetrics position, double offset) {
-    // print('applyPhysicsToUserOffset $offset');
-    // print("");
-    // print(
-    //     'applyBoundaryConditions \npixels ${position.pixels} \nminScrollExtend ${position.minScrollExtent} \nmaxScrollExtend ${position.maxScrollExtent} \nvieweportDimension ${position.viewportDimension} \nextendBefore ${position.extentBefore} \nextendsAfter ${position.extentAfter} \nextendsInside ${position.extentInside} \noutOfRange ${position.outOfRange}');
-    // print("");
-    final a = FixedScrollMetrics(
-        maxScrollExtent: position.maxScrollExtent,
-        viewportDimension: 10000,
-        pixels: position.pixels,
-        minScrollExtent: position.minScrollExtent,
-        axisDirection: position.axisDirection);
-    return super.applyPhysicsToUserOffset(position, offset);
-  }
-
-  @override
-  Simulation createBallisticSimulation(
-      ScrollMetrics position, double velocity) {
-    print('BALLISTICS');
-    return super.createBallisticSimulation(position, velocity);
-  }
-
-  @override
-  bool shouldAcceptUserOffset(ScrollMetrics position) => true;
 }
